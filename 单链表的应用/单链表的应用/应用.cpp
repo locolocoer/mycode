1a9a35a5176e42d2450869691f16cc26a17ecca6
locolocoer <1457901147@qq.com>
2021-02-28 13:33:47 +0800

    'second_commit'

--- 单链表的应用/单链表的应用/应用.cpp
+++ 单链表的应用/单链表的应用/应用.cpp
@@ -1,8 +1,24 @@
+#include <malloc.h>
 typedef struct LNode {
 	struct LNode* next;
 	int data;
 }LinkNode;
 void split(LinkNode*& L, LinkNode*& L1, LinkNode*& L2)
 {
-	LinkNode* p = L->next;
+	LinkNode* p = L->next,*q,*r1;
+	L1 = L;
+	r1 = L1;
+	L2 = (LinkNode*)malloc(sizeof(LinkNode));
+	L2->next = NULL;
+	while(p!=NULL)
+	{
+		r1->next = p;
+		r1 = p;
+		p = p->next;
+		q = p->next;
+		p->next = L2->next;
+		L2->next = p;
+		p = q;
+	}
+	r1->next = NULL;
 }
