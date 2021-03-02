1f74b55c0c187723d1654886e5dbb5ca5dd78422
locolocoer <1457901147@qq.com>
2021-03-02 21:18:54 +0800

    'first'

---
+++ 栈/栈/stack.h
@@ -0,0 +1,44 @@
+#pragma once
+#define MAXSIZE 1000
+#include <malloc.h>
+#include <stdio.h>
+typedef int ElemType;
+typedef struct {
+	ElemType data[MAXSIZE];
+	int top;
+}SqStack;
+void InitStack(SqStack*& s) {
+	s = (SqStack*)malloc(sizeof(SqStack));
+	s->top = -1;
+}
+void DestoryStack(SqStack *&s)
+{
+	free(s);
+}
+bool StackEmpty(SqStack* s)
+{
+	return (s->top == -1);
+}
+bool Push(SqStack*& s,ElemType e)
+{
+	if (s->top == MAXSIZE - 1)
+		return false;
+	s->top++;
+	s->data[s->top] = e;
+	return true;
+}
+bool Pop(SqStack*& s, ElemType& e)
+{
+	if (s->top == -1)
+		return false;
+	e = s->data[s->top];
+	s->top--;
+	return true;
+}
+bool GetTop(SqStack* s, ElemType& e)
+{
+	if (s->top == -1)
+		return false;
+	e = s->data[s->top];
+	return true;
+}
--- 求解迷宫问题/求解迷宫问题/源.cpp
+++ 求解迷宫问题/求解迷宫问题/源.cpp
@@ -4,33 +4,138 @@
 #include <stdio.h>
 #include <malloc.h>
 int mg[M + 2][N + 2] = { {1,1,1,1,1,1,1,1,1,1},
-				  {1,0,0,1,0,0,0,1,0,1},
-				  {1,0,0,1,0,0,0,1,0,1},
-				  {1,0,0,0,0,1,1,0,0,1},
-				  {1,0,1,1,1,0,0,0,0,1},
-				  {1,0,0,0,1,0,0,0,0,1},
-				  {1,0,1,0,0,0,1,0,0,1},
-				  {1,0,1,1,1,0,1,1,0,1},
-				  {1,1,0,0,0,0,0,0,0,1},
-				  {1,1,1,1,1,1,1,1,1,1}
+				         {1,0,0,1,0,0,0,1,0,1},
+				         {1,0,0,1,0,0,0,1,0,1},
+				         {1,0,0,0,0,1,1,0,0,1},
+				         {1,0,1,1,1,0,0,0,0,1},
+				         {1,0,0,0,1,0,0,0,0,1},
+				         {1,0,1,0,0,0,1,0,0,1},
+				         {1,0,1,1,1,0,1,1,0,1},
+				         {1,1,0,0,0,0,0,0,0,1},
+				         {1,1,1,1,1,1,1,1,1,1}
 };
+
 typedef struct
 {
 	int i;
 	int j;
 	int di;
 }Box;
+typedef Box ElemType;
 typedef struct {
 	Box data[MAXSIZE];
 	int top;
-}sttype;
+}SqStack;
+
+void InitStack(SqStack*& s) 
+{
+	s = (SqStack*)malloc(sizeof(SqStack));
+	s->top = -1;
+}
+void DestoryStack(SqStack*& s)
+{
+	free(s);
+}
+bool StackEmpty(SqStack* s)
+{
+	return (s->top == -1);
+}
+bool Push(SqStack*& s, ElemType e)
+{
+	if (s->top == MAXSIZE - 1)
+		return false;
+	s->top++;
+	s->data[s->top] = e;
+	return true;
+}
+bool Pop(SqStack*& s, ElemType& e)
+{
+	if (s->top == -1)
+		return false;
+	e = s->data[s->top];
+	s->top--;
+	return true;
+}
+bool GetTop(SqStack* s, ElemType& e)
+{
+	if (s->top == -1)
+		return false;
+	e = s->data[s->top];
+	return true;
+}
 
 bool mgpath(int xi, int yi, int xe, int ye)
 {
 	Box path[MAXSIZE], e;
 	int i, j, di, il, jl, k;
 	bool find;
-	sttype* st;
-	st = (sttype*)malloc(sizeof(sttype));
-	st->top = -1;
+	SqStack* st;
+	InitStack(st);
+	e.i = xi;
+	e.j = yi;
+	e.di = -1;
+	Push(st, e);
+	mg[xi][yi] = -1;
+	while (!StackEmpty(st))
+	{
+		GetTop(st, e);
+		i = e.i;
+		j = e.j;
+		di = e.di;
+		if (i == xe && j == ye)
+		{
+			k = 0;
+			while (!StackEmpty)
+			{
+				Pop(st, e);
+				path[k++] = e;
+			}
+			while (k >= 1)
+			{
+				k--;
+				printf("\t(%d,%d)", path[i].i, path[k].j);
+				if ((k + 2) % 5 == 0)
+					printf("\n");
+
+			}
+			printf("\n");
+			DestoryStack(st);
+			return true;
+		}
+		find = false;
+		while (di < 4 && !find)
+		{
+			di++;
+			switch (di)
+			{
+			case 0:il = i - 1; jl = j; break;
+			case 1:il = i; jl = j + 1; break;
+			case 2:il = i + 1; jl = j; break; 
+			case 3:il = i; jl = j - 1; break;
+			}
+			if (mg[il][jl] == 0)
+				find == true;
+		}
+		if (find)
+		{
+			st->data[st->top].di == di;
+			e.i = il;
+			e.j = jl;
+			e, di = -1;
+			Push(st, e);
+			mg[il][jl] = -1;
+		}
+		else
+		{
+			Pop(st, e);
+			mg[e.i][e.j] = 0;
+		}
+	}
+	DestoryStack(st);
+	return false;
+}
+int main() {
+	if (!mgpath(1, 1, M, N))
+		printf("ц╩сп╫Б");
+	return 1;
 }
