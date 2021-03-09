96cff216d3641dd760bda1fc090c83cb75c14dc8
locolocoer <1457901147@qq.com>
2021-03-09 16:19:10 +0800

    'first'

---
+++ 图的邻接表/图的邻接表/adjacency.h
@@ -0,0 +1,19 @@
+#pragma once
+#define MAXV 100
+typedef struct ANode
+{
+	int adjvex;
+	struct ANode* nextarc;
+	int weight;
+}ArcNode;
+typedef struct Vnode
+{
+	int data;
+	ArcNode* firstarc;
+}VNode;
+typedef struct {
+	VNode adjlist[MAXV];
+	int n, e;
+
+}AdjGraph;
+void CreateGraph(AdjGraph*& G, int A[MAXV][MAXV], int n, int e);
